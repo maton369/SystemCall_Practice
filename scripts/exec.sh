7fd51a1a342bb0467466d9e8f80d31594bd3c3c2
maton369 <ysk901085@gmail.com>
2025-06-08 15:35:52 +0900

    Add: _exitでプロセスが終了することを確認

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -5,5 +5,9 @@
 # gcc -o fork fork.c
 # ./fork | tee output.log
 
-gcc -o execve execve.c
-./execve | tee output.log
+# gcc -o execve execve.c
+# ./execve | tee output.log
+
+gcc -o exit exit.c
+./exit | tee output.log
+echo $? |tee -a output.log
